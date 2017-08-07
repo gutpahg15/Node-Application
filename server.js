fba3cc8c2fd5e309b517ca8fb7f333d40cf4de0e
Thangavelu <Jagadeesh.Thangavelu@Mattel.com>
2017-08-07 16:26:25 -0400

    heroku port added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -25,4 +25,6 @@ app.get('/bad', (req, res) => {
 res.send({errorMessage : 'Unable to load the data'});
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`);
+});
