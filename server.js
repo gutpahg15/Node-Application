251a3bf0e02846807ce4f5c58e588f2e49f7d7d8
Thangavelu <Jagadeesh.Thangavelu@Mattel.com>
2017-08-07 15:15:16 -0400

    node application

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+res.render('home.hbs', {
+  pageTitle : 'Home Page',
+  Welcome : 'Welcome to homepage'
+});
+});
+
+app.get('/about', (req, res) => {
+res.render('about.hbs', {
+  pageTitle : 'About This Page',
+  Welcome : 'Welcome to about page'
+});
+});
+
+app.get('/bad', (req, res) => {
+res.send({errorMessage : 'Unable to load the data'});
+});
+
+app.listen(3000);
